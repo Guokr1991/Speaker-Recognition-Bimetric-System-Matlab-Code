4675480d65a18d8d3d4ef445f502ae16fa33a065
Hamdi Boukamcha <hamdouchhd@hotmail.com>
2014-12-20 17:21:37 +0100

    Speaker Recognition System Biometric
    Speaker Recognition System V3 : Simple and Effective Source Code For for
    Speaker Identification Based On Neural Networks.

---
+++ sourcecode.m
@@ -0,0 +1,34 @@
+% 
+%Speaker Recognition based on Neural Networks
+% 
+% In order to obtain the complete source code for Speaker Recognition based on 
+% Neural Networks please visit my website
+% 
+% http://matlab-recognition-code.com/speaker-recognition-biometric-system-matlab-code/
+%
+% 
+% A small donation (just 20 U.S. Dollars) is required.
+% 
+% Release      Date           Major features
+% 
+% 14-06-2014         3.0        - Speaker recognition based on Neural Networks
+%                               - High recognition rate
+%                               - Text-independent
+%                               - Fully customizable
+%                               - Easy c/c++ implementation
+%                               - Sampling frequency-independent
+%                               - Easy and intuitive GUI
+%
+% For more informations please email me (hamdouchhd@hotmail.com)
+%
+% Hamdi Boukamcha
+% Tunisia
+% Sousse
+% 4081
+% email  hamdouchhd@hotmail.com
+% mobile +216 50674269
+% website http://matlab-recognition-code.com
+%
+%
+%
+%
